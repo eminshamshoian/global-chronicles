f2c86d90e24f15b3445994a2b41427baaa4e2b90
eminshamshoian <47337592+eminshamshoian@users.noreply.github.com>
2022-06-11 17:48:31 -0700

    changed posts border to shadow

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,11 +63,11 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
-      <div className='container mx-auto'>
+      <div className='container mx-auto py-5'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
           {posts.map((post) => (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div className='group cursor-pointer border rounded-lg overflow-hidden'>
+              <div className='group cursor-pointer shadow-lg rounded overflow-hidden'>
                 <img
                   className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
                   src={urlFor(post.mainImage).url()!}
