156080d45a9cf686d6d6f6d352191f93aa10d238
eminshamshoian <47337592+eminshamshoian@users.noreply.github.com>
2022-06-12 14:05:30 -0700

    changed CTA

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,9 @@ export default function Home({ posts }: Props) {
       <div className='bg-red-100'>
         <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between'>
           <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-            <span className='block'>Want to learn more about medium?</span>
+            <span className='block'>
+              Want to learn more about global chronicles?
+            </span>
             <span className='block black'>Subscribe for more.</span>
           </h2>
           <div className='mt-8 flex lg:mt-0 lg:flex-shrink-0'>
